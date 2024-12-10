6c5f32daa11d5efc14173768021840afb5e1815b
Naveed786Ahmed <138322439+Naveed786Ahmed@users.noreply.github.com>
2024-12-10 22:39:04 +0500

    fix the cors function error

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors"
 
 const app = express()
 dotenv.config()
-app.use(cors)
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.status(200).send("Welcome To DevDocket API")
